9ec93b012ff8096e8ce679052c62360e23f8bc75
AbiramiDevi-28 <160620195+AbiramiDevi-28@users.noreply.github.com>
2024-02-24 10:47:17 +0200

    Update SecondProject.java

--- SecondProject.java
+++ SecondProject.java
@@ -5,6 +5,6 @@ public static void main(String[] args)
  System.out.println("Heloo Aaru");
  System.out.println("Heloo Aadhi");
 System.out.println("Heloo Veeraganesan");
- System.out.println("Heloo AbiramiDevi C");
+ System.out.println("Heloo AbiramiDeviC");
 }
 }
