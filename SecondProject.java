65ce61e36c8c3ef69218efe2fb2126a72c0ed7f1
AbiramiDevi-28 <160620195+AbiramiDevi-28@users.noreply.github.com>
2024-02-24 09:47:27 +0200

    TestJenkins

--- SecondProject.java
+++ SecondProject.java
@@ -5,5 +5,6 @@ public static void main(String[] args)
  System.out.println("Heloo Aaru");
  System.out.println("Heloo Aadhi");
 System.out.println("Heloo Veera");
+ System.out.println("Heloo Veera");
 }
 }
